f4d8e7f3a6310e2c5bd802df0526e9c432b21414
henry <henrybosch15@gmail.com>
2025-07-10 22:36:36 -0700

    remove extra stuff

--- src/main.py
+++ src/main.py
@@ -1,11 +1,5 @@
 def main():
     print("Hello from computational-pwidths!")
 
-
-def add_numbers(a, b):
-    """Simple function to add two numbers."""
-    return a + b
-
-
 if __name__ == "__main__":
     main()
